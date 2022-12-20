d480de8709b21b9b1a8a85ff733b3e485319dd7b
Yatharth Meghwal <89915302+YatharthMeghwal@users.noreply.github.com>
2022-12-20 16:10:32 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,20 @@
-function addUser() {
+const firebaseConfig = {
+    apiKey: "AIzaSyCPLvW-jwSplebLdcYwIY784LxP2ccKvc4",
+    authDomain: "lets-chat-3ca15.firebaseapp.com",
+    projectId: "lets-chat-3ca15",
+    storageBucket: "lets-chat-3ca15.appspot.com",
+    messagingSenderId: "261484146348",
+    appId: "1:261484146348:web:905e3396be226d713a3909",
+    measurementId: "G-EMSDLPFNWH"
+  };
+      
+      // Initialize Firebase
+     firebase.initializeApp(firebaseConfig);
+
+
+     function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name",user_name);
     window.location = "kwitter_room.html";
 }
+
